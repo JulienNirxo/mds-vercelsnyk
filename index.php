d28030f63057ef17eef28a38586d0002f3ede053
JulienNirxo <millaud.julien@gmail.com>
2023-05-30 11:56:58 +0200

    [init]

---
+++ index.php
@@ -0,0 +1,18 @@
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Home</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+            </div>
+        </div>
+    </div>
+</body>
+<script>
+    document.querySelector('h1').textContent = 'Home Page';
+</script>
