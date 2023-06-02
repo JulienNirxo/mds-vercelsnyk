d3308a5df0daeedf8338b07e880c93d30a063402
JulienNirxo <millaud.julien@gmail.com>
2023-06-02 09:28:58 +0200

    [FIX] new test

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ console.log('salut');
 console.log('le js c cool');
 console.log('le js c cool et marrant');
 console.log('le js c cool et marrant et facile');
+console.log('le js c cool et marrant');
+console.log('le js c cool et marrant et facile');
